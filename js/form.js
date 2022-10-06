83780737e4c05357aad8d75e34711a1e8e0dc8a7
emangelina <tas.zaitseva@gmail.com>
2022-10-06 13:31:28 +0300

    стилизовала блок формы, сделала адаптивность

---
+++ js/form.js
@@ -0,0 +1,11 @@
+
+  let form = document.querySelector('#form');
+  
+  let priceInput = form.elements.priceInput;
+  let priceScale = form.elements.priceScale;
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  })
+
+
